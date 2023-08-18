e9734943708809076f8b67a1aa499c9891c41986
Arif Hossan <arif.hossan.eu@gmail.com>
2023-08-18 21:52:12 +0600

    pass dynamic data using event handler

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Cosmetic = (props) => {
-    const {name,price} = props.cosmetic;
+    const {id,name,price} = props.cosmetic;
+    const addToCart = (id) =>{
+        console.log('item added',id);
+    }
+    const addCart = () => addToCart(id);
     return (
         <div>
             <h4>Buy Now : {name}</h4>
             <p>Price : {price}</p>
+            <button onClick={addCart}>Add To Cart</button>
+            {/* shortcut and conventional way to pass data using event handler */}
+            <button onClick={()=>addToCart(id)}>Purchase</button>
         </div>
     );
 };
